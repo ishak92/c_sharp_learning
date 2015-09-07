dc297b2bff9f31a4a3db1930873162c7f65e4978
ishak92 <ishak92@yandex.ru>
2015-09-07 20:51:11 +0300

    2nd

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -8,12 +8,23 @@ namespace CSharpLearning
 {
     class Program
     {
+        static int square(int n)
+        {
+            return n * n;
+        }
+
         static void Main(string[] args)
         {
             int i = 0;
             while(i < 10)
             {
                 Console.WriteLine(i * i);
+                i++;
+            }
+            i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine( square(i) );
                 i++;
             }
         }
